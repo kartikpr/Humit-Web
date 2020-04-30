230939d66bf1d5d0047c33de783cae51120f7978
Kartik Prakash <karu@Kartiks-MacBook-Pro.local>
2020-04-30 22:55:07 +0530

    Heroku deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import Typewriter from 't-writer.js';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Humit';
+
+    constructor(private router: Router) { }
+
+   ngOnInit() {
+     const target = document.querySelector('.tw')
+
+  const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: 'white',
+      typeSpeed : 100,
+      deleteSpeed : 200,
+      cursorColor: '#9592FF'
+
+    })
+
+    writer.type('discovery').rest(500).remove(9).type('sharing').rest(200).start()
+
+
+}
+
+gotoForm(){
+  this.router.navigate(['https://forms.gle/f4sT8yZabUYRjEEa7']);
+}
+
 }
